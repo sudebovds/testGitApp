560b7dece5640d948849dc61fe2a934a714c1315
Mit Sudebov <sudebovds@gmail.com>
2021-10-01 11:25:04 +0300

    simplified app

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,35 +1,49 @@
 import axios from 'axios'
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container, Row, Col } from 'react-bootstrap';
-import Index from './Pages/Index';
+import SearchForm from './Components/SearchForm/SearchForm';
+import { ContextInterface } from './Interfaces/Common'
 
-interface ContextInterface {
-  totalCount: number;
-  items: {
-      node_id: string;
-      name: string;
-      full_name: string;
-      owner: {
-          login: string;
-      };
-      description: string;
-      open_issues: number;
-      forks: number;
-  }[]
-}
 
 const Home = () => {
-  const [data, setData] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(true);
+  const [data, setData] = useState<ContextInterface | null>(null);
 
     return(
       <Container fluid className = 'wrapper'>
         <Row>
           <Col>
-            <Index />
+            <h1 className = 'text-center'>Find Repos</h1>
           </Col>
         </Row>
+        <SearchForm
+          data = {data}
+          setData = {setData}
+        />
+        {data ? 
+          <Row>
+            <Col>
+              <div className="list-group">
+                {
+                  data?.items.map((item: any) => {
+                    return(
+                      <a key = {item.node_id} href="#" className="list-group-item list-group-item-action" aria-current="true">
+                        <div>
+                          <h2>{item.name}</h2>
+                          <p>Autor: {item.owner.login}</p>
+                          <p>{item.description}</p>
+                          <div>
+                            {item.watchers} {item.open_issues} {item.forks}
+                          </div>
+                        </div>
+                      </a>
+                    )
+                    })
+                }
+              </div>
+            </Col>
+          </Row>
+        : null}
       </Container>
     )
 
